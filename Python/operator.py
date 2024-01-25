57da4657d9c2f3c411699d642e4851a0c346bb22
backdfb <139917593+backdfb@users.noreply.github.com>
2024-01-25 23:22:31 +0900

    Create operator.py

---
+++ Python/operator.py
@@ -0,0 +1,29 @@
+playing = True
+
+while playing:
+    a = int(input("Choose a number:\n"))
+    b = int(input("Choose another one:\n"))
+    operation = input("Choose an operation:\n    Options are: +, -, *, or /.\n    Write 'exit' to finish.\n")
+
+    if operation == "+":
+        result = a + b
+        print(result)
+    elif operation == "-":
+        result = a - b
+        print(result)
+    elif operation == "*":
+        result = a * b
+        print(result)
+    elif operation == "/":
+        if b == 0:
+            print("Don't Use 0 in another one")
+        else:
+            result = a / b
+            print(result)
+    elif operation == "exit":
+        print("See You Again.")
+        playing = False
+    else:
+        print("Retry.")
+
+print()
